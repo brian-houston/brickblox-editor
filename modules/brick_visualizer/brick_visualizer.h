1ace9cacadd09e142e5c93268f840a9e2fd5d05b
Brian Houston <>
2025-02-28 18:25:07 -0500

    create basic brick visualizer interface

--- modules/brick_visualizer/brick_visualizer.h
+++ modules/brick_visualizer/brick_visualizer.h
@@ -2,7 +2,18 @@
 #define BRICK_VISUALIZER_H
 
 #include "scene/3d/multimesh_instance_3d.h"
+
 class BrickVisualizer : public MultiMeshInstance3D {
     GDCLASS(BrickVisualizer, MultiMeshInstance3D);
+public:
+    BrickVisualizer();
+    void setup(int p_max_instances);
+    int add_instance(Transform3D p_transform);
+    void remove_instance(int p_id);
+    void set_instance_transform(int p_id, Transform3D p_transform);
+private:
+    int max_instances;
+    int current_instances;
+    LocalVector<int> available_slots;
 };
 #endif
