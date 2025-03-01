7728b1fc9222ffb06bfacc55a1b465beea65ea19
Brian Houston <>
2025-02-28 21:41:23 -0500

    create brick node

---
+++ modules/brick/register_types.cpp
@@ -0,0 +1,17 @@
+#include "register_types.h"
+
+#include "core/object/class_db.h"
+#include "brick.h"
+
+void initialize_brick_module(ModuleInitializationLevel p_level) {
+	if (p_level != MODULE_INITIALIZATION_LEVEL_SCENE) {
+		return;
+	}
+	ClassDB::register_class<Brick>();
+}
+
+void uninitialize_brick_module(ModuleInitializationLevel p_level) {
+	if (p_level != MODULE_INITIALIZATION_LEVEL_SCENE) {
+		return;
+	}
+}
